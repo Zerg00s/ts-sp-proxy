f3c7eae6d810f0fbd201c0f8c9829709746f3632
Denis Molodtsov <molodtsovdenis@gmail.com>
2018-05-06 23:08:42 -0400

    added react-router-dom that Works

--- src/About.tsx
+++ src/About.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './App.scss';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+class About extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">About</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          ABOUT THIS PAGE: nothing to see here :)
         </p>
-        <Link to='/about'>About</Link>
+        <Link to='/'>Home</Link>
       </div>
     );
   }
 }
 
-export default App;
+export default About;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import About from './About';
 import App from './App';
 import './index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <HashRouter  >
+    <Switch>
+      <Route exact={true} path="/" component={App} />
+      <Route exact={true} path="/about" component={About} />
+    </Switch>
+  </HashRouter>
+  ,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
